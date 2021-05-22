ba9f30b6ec266b0e1b628b58395ce2acbb3691cf
Dariusz Filipiak <dariusz.filipiak@igt.com>
2021-05-22 04:58:09 +0200

    a

---
+++ environment.js
@@ -0,0 +1,11 @@
+
+const path = require('path')
+var { config } = { config: {} };
+
+config = {
+
+    domain: 'tnp-tools.example.domain.com',
+
+
+}
+module.exports = exports = { config };
---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import { Helpers } from 'tnp-config';
+if (Helpers.isBrowser) {
+  console.log(`This package is only for node backend`);
+}
+//#region @backend
+import { child_process } from 'tnp-core';
+import { ConfigModels, GlobalIsomorphicDependencies } from 'tnp-config';
+import chalk from 'chalk';
+import * as commandExist from 'command-exists';
+const commandExistsSync = commandExist.sync;
+import isElevated from 'is-elevated';
+
+export class CLI {
+
+  public static isElevated = isElevated;
+  public static commandExistsSync = commandExistsSync;
+  public static chalk = chalk;
+  /**
+   * Check if global system tools are available for isomorphic app development
+   */
+  public static checkEnvironment(globalDependencies: ConfigModels.GlobalDependencies = GlobalIsomorphicDependencies) {
+    const missingNpm: ConfigModels.GlobalNpmDependency[] = [];
+    globalDependencies.npm.forEach(pkg => {
+      if (!commandExistsSync(pkg.name)) {
+        missingNpm.push(pkg)
+      }
+    })
+
+    if (missingNpm.length > 0) {
+
+      const toInstall = missingNpm
+        .map(pkg => {
+          const n = pkg.installName ? pkg.installName : pkg.name;
+          return pkg.version ? `${n}@${pkg.version}` : n;
+        })
+        .join(' ');
+      console.log(chalk.red(`Missing npm dependencies.`))
+      const cmd = `npm install -g ${toInstall}`;
+      console.log(`Please run: ${chalk.green(cmd)}`)
+      process.exit(0)
+    }
+
+    globalDependencies.programs.forEach(p => {
+      if (!commandExistsSync(p.name)) {
+        console.log(chalk.red(`Missing command line tool "${p.name}".`))
+        console.log(`Please install it from: ${chalk.green(p.website)}`)
+        process.exit(0)
+      }
+    })
+
+
+    try {
+      child_process.execSync(`check-node-version --node ">= 9.2"`, { stdio: [0, 1, 2] })
+    } catch (error) {
+      process.exit(0)
+    }
+  }
+}
+
+//#endregion
